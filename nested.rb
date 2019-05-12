14bc055e8dfac77f1b02c80268d191ef285d161e
Oana Berentan <oana.berentan@yahoo.com>
2019-05-12 22:18:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -97,12 +97,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      programmer_hash[:alan_kay][:known_for] = "GUI"
 		 return programmer_hash
-
-
 end
+
 
 def adding_to_dennis
 	programmer_hash =
@@ -120,5 +118,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] >> "Assembly"
 
 end
