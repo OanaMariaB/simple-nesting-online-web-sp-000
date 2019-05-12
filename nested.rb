0b4119f3b9cad7c0a636c18a529c715b034f3750
Oana Berentan <oana.berentan@yahoo.com>
2019-05-12 22:09:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
+        }
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
